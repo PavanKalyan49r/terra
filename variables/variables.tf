43b66ae43dd123926d4d2d8839302c171a4a30e3
PavanKalyan49r <pavan49r@gmail.com>
2024-07-19 10:15:21 +0530

    terraform practice

--- variables/variables.tf
+++ variables/variables.tf
@@ -15,3 +15,23 @@ variable "tags" {
     projrct= "roboshop"
   }
 }
+
+variable "sg-name" {
+  type = string
+  default = "roboshop-all"
+}
+
+variable "sg-description" {
+  type = string
+  default = "alllowing all ports"
+}
+
+variable "inbound-from-port" {
+  type = number
+  default = 0
+}
+
+variable "cidr_blocks" {
+    type = list
+    default = ["0.0.0.0/0"]
+}
